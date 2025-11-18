fac174f417d71828a3ceef6ffca02283940e8ceb
turner <douglass.turner@gmail.com>
2025-11-18 15:07:31 -0500

    tweaks: camera fov. zoom range

--- src/constants.js
+++ src/constants.js
@@ -4,19 +4,19 @@
 export const CONFIG = {
   // Camera settings
   CAMERA: {
-    FOV: 75,
+    FOV: 32,
     NEAR: 0.1,
     FAR: 1000,
-    INITIAL_Z: 5,
-    MIN_ZOOM: 2,
-    MAX_ZOOM: 20,
+    INITIAL_Z: 32,
+    MIN_ZOOM: 8,
+    MAX_ZOOM: 64,
     ZOOM_SPEED: 0.1,
   },
 
   // Model settings
   MODEL: {
     SIZE: 1,
-    COLOR: 0x00ff00,
+    COLOR: 0xff0000,
   },
 
   // Lighting settings
